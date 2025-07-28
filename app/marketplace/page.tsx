77d2c883b96474a6ddd9c9b33273e02eb8cd4b40
54simple <fiftyfoursimple10@gmail.com>
2025-07-28 17:28:32 +0300

    COMMON TOUCH

--- app/marketplace/page.tsx
+++
@@ -1,7 +0,0 @@
-import SearchDiscoveryPage from "@/components/parts/myafrika/market-item/search-discovery-page";
-
-export default function MarketPlacePage() {
-    return (
-        <SearchDiscoveryPage/>
-    )
-}
--- app/register/page.tsx
+++
@@ -1,7 +0,0 @@
-import BusinessRegistration from "@/components/parts/registration/business-registration";
-
-export default function RegisterPage() {
-    return (
-        <BusinessRegistration/>
-    )
-}
